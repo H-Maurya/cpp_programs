640d7bf3c51008ee31bfb5f2e052b92f45b0b4aa
Harshit <hmaurya109@gmail.com>
2022-05-19 12:20:46 +0530

    1

---
+++ print_1_to_n.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(int n){
+	if(n==1){
+		cout<<1<<" ";
+	}
+	else{
+		print(n-1);
+		cout<<n<<" ";
+	}
+}
+int main(){
+	int n=10;
+	print(n);
+	return 0;
+}
