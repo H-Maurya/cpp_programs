eb0bf1b842a1c46c5c701d5c67a18599be49091c
Harshit <hmaurya109@gmail.com>
2022-05-19 13:00:33 +0530

    2

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+	if(n==0){
+		return 1;
+	}
+	if(n<0){
+		return -1;
+	}
+	return n*factorial(n-1);
+}
+int main(){
+	int n=8;
+	cout<<factorial(n);
+	return 0;
+}
---
+++ Recursion/height_of_tree.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+	int key;
+	node* left;
+	node* right;
+};
+
+struct node* newNode(int val) {
+	node* n = new node;
+	n->key = val;
+	n->left = NULL;
+	n->right = NULL;
+	return n;
+};
+
+int height(node* n) {
+	if(n==NULL){
+		return 0;
+	}
+	int l = height(n->left);
+	int r = height(n->right);
+	return 1+max(l,r);
+}
+
+int main(){
+	struct node *a, *b, *c, *d, *e, *f;
+	a=newNode(1);
+	b=newNode(2);
+	c=newNode(3);
+	d=newNode(4);
+	e=newNode(5);
+	f=newNode(6);
+	a->left = b;
+	a->right = c;
+	b->left = d;
+	c->left = e;
+	d->left = f;
+
+	cout<<height(a);
+
+	return 0;
+}
+
+
+/*
+			1
+		  /	  \
+		 2     3
+	    /     / 
+	   4     5   
+	  /
+	 6
+
+*/
