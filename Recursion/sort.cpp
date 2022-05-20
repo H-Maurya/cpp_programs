05df4f3c9e3916915a7257769660d46059a1648e
Harshit <hmaurya109@gmail.com>
2022-05-20 12:29:26 +0530

    3

---
+++ Recursion/sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insert(vector<int>& v, int temp){
+	if(v.size()==0 || v[v.size()-1]<=temp){
+		v.push_back(temp);
+		return;
+	}
+	else{
+		int val = v[v.size()-1];
+		v.pop_back();
+		insert(v, temp);
+		v.push_back(val);
+		return;
+	}
+}
+
+void sort(vector<int>& v){
+	if(v.size()<=1){
+		return;
+	}
+	else{
+		int val=v[v.size()-1];
+		v.pop_back();
+		sort(v);
+		insert(v,val);
+		return;
+	}
+}
+
+int main(){
+	vector<int> v = {1,0,4,2,6};
+	sort(v);
+	for(int it:v){
+		cout<<it<<" ";
+	}
+	return 0;
+}
