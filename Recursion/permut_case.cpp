d11189bebdfcb37064f92af56b542455a278e8e0
Harshit <hmaurya109@gmail.com>
2022-05-22 15:55:31 +0530

    5

---
+++ Recursion/permut_case.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char change_case (char c) {
+    if (isupper(c)) 
+        return tolower(c); 
+    else
+        return toupper(c); 
+}
+
+void solve(string ip, string op){
+	if(ip.length()==0){
+		cout<<op<<"\n";
+		return;
+	}
+	else{
+		string op1 = op+ip[0];
+		string op2 = op+change_case(ip[0]);
+		ip.erase(ip.begin()+0);
+		solve(ip, op1);
+		solve(ip, op2);
+		return;
+	}
+
+}
+
+
+int main(){
+	string ip = "ABc";
+	string op = "";
+	solve(ip, op);
+
+	return 0;
+}
---
+++ Recursion/permut_digit.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> v;
+
+bool isDigit(char c){
+	if(c>='0' && c<='9') {
+		return true;
+	}
+	return false;
+}
+
+char change_case (char c) {
+    if (isupper(c)) 
+        return tolower(c); 
+    else
+        return toupper(c); 
+}
+
+void solve(string ip, string op){
+	if(ip.length()==0){
+		v.push_back(op);
+		return;
+	}
+	else{
+		if(isDigit(ip[0])){
+			string op1 = op;
+			while(isDigit(ip[0])){
+				op1.push_back(ip[0]);
+				ip.erase(ip.begin()+0);
+			}
+			solve(ip, op1);
+			return;
+		}
+		else{
+			string op1 = op+ip[0];
+			string op2 = op+change_case(ip[0]);
+			ip.erase(ip.begin()+0);
+			solve(ip, op1);
+			solve(ip, op2);
+			return;
+		}
+		
+	}
+
+}
+int main(){
+	string ip = "a1B2";
+	string op = "";
+	solve(ip, op);
+	for(auto it:v){
+		cout<<it<<"\n";
+	}
+	return 0;
+}
