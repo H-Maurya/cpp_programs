ef6946cc68bd079fc205359b0a53d7429edd2313
Harshit <hmaurya109@gmail.com>
2022-06-03 14:08:58 +0530

    DP8

---
+++ DP/coin_change_min.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> coin, int n, int sum, vector<vector<int>> &t){
+	for(int i=2; i<n+1; i++){
+		for(int j=1; j<sum+1; j++){
+			if(coin[i-1]<=j){
+				t[i][j] = min(1+t[i][j-coin[i-1]], t[i-1][j]);
+			}
+			else{
+				t[i][j] = t[i-1][j];
+			}
+		}
+	}
+	return t[n][sum];
+}
+
+int main(){
+	int n = 3;
+	vector<int> coin = {8,1,2};
+	int sum = 5;
+	vector<vector<int>> t(n+1, vector<int> (sum+1, INT_MAX-1));
+	for(int i=1; i<n+1; i++){
+		t[i][0] = 0;
+	}
+
+	//Initialise 2nd row from index 1 manually.
+	for(int j=1; j<sum+1; j++){
+		if(j%coin[0]==0){
+			t[1][j] = j/coin[0];
+		}
+		else{
+			t[1][j] = INT_MAX-1;
+		}
+	}
+	cout<<solve(coin, n, sum, t)<<"\n";
+	return 0;
+}
---
+++ DP/rod.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> l, vector<int> p, int n, vector<vector<int>> &t){
+	for(int i=1; i<n+1; i++){
+		for(int j=1; j<n+1; j++){
+			if(l[i-1]<=j){
+				t[i][j] = max(p[i-1]+t[i][j-l[i-1]], t[i-1][j]);
+			}
+			else{
+				t[i][j] = t[i-1][j];
+			}
+		}
+	}
+	return t[n][n];
+}
+
+int main(){
+	int n = 8;
+	vector<int> l = {1,2,3,4,5,6,7,8};
+	vector<int> p = {1,5,8,9,10,17,17,20};
+	vector<vector<int>> t(n+1, vector<int> (n+1, 0));
+	cout<<solve(l, p, n, t)<<"\n";
+
+	return 0;
+}
